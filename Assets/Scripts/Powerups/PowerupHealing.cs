a4c2896d5c144c473824f24f2dcd6c2a372d6a7b
JimmyIFP <ifp.jimmy@gmail.com>
2026-01-20 19:20:18 +0100

    Actualizacion de la rama 'feature/map-decoration' con rebase

---
+++ Assets/Scripts/Powerups/PowerupHealing.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PowerupHealing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
